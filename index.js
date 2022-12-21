5dfa85cffdeea1864371207d11f2de5e1428d164
Ashu73895 <ashutoshvermadec1995@gmail.com>
2022-12-21 21:58:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log("Hello world");
+// var myName = 'Ritesh kumar';
+// console.log(myName);
+// var myAge=26;
+// console.log(myAge);
+
+// const a = {
+//     name:"Ritesh",
+//     section:1,
+//     isPrinciple:false
+// }
+// a['friend']="shubham"
+// a['name']="lovish"
+// console.log(a)
+
+const dict = {
+    comprehensive:"covering or involving much",
+    intuition:"direct perception of truth",
+    perception:"apprehending by means of the senses or of the mind",
+    compile:"to put together (documents, selections, or other materials) in one book or work",
+    absolute:"free from imperfection"
+}
+
+console.log(dict.compile)
